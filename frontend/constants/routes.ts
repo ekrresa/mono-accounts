366393d2d93d7d5969945755417c13ee3d96466b
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2021-11-19 23:45:45 +0100

    Built layout

---
+++ frontend/constants/routes.ts
@@ -0,0 +1,8 @@
+export const DASHBOARD: Record<string, string> = {
+	Dashboard: '/',
+	Expenses: '/expenses',
+	Wallets: '/wallets',
+	Summary: '/summary',
+	Accounts: '/accounts',
+	Settings: '/settings',
+};
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,3 +1,21 @@
+import { DASHBOARD } from '../constants/routes';
+import LogoLight from '../public/images/logo-light.svg';
+
 export default function Home() {
-  return <div className=""></div>;
+	return (
+		<main className="flex">
+			<aside className="bg-black-100 sticky top-0 left-0 flex-col flex-shrink-0 h-screen px-16 overflow-y-auto w-72">
+				<div className="py-16">
+					<LogoLight className="w-32" />
+				</div>
+
+				<div className="opacity-50 text-white">
+					{Object.keys(DASHBOARD).map(key => (
+						<div className="pb-8 text-[1.375rem] last:pb-0">{key}</div>
+					))}
+				</div>
+			</aside>
+			<section className="flex-1"></section>
+		</main>
+	);
 }
